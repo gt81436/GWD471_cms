c1ac12784f72bdb51feec12145920cfe50cb1cf2
gt81436 <george.trammell@yahoo.com>
2014-04-09 20:30:52 -0400

    Initial index page creation

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>George Trammell Media</title>
+<link rel="stylesheet" href="main.css" />
+</head>
+
+<body>
+	<header>
+    	<h1>Welcome to George Trammell Media!</h1>
+        <nav>
+        	<a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+        </nav>
+    </header>
+    <section>
+    	<h2>Intro</h2>
+        <p>
+        <img src="" alt="my pic" />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis purus, pharetra eu ante nec, sollicitudin interdum tortor. Suspendisse in dui lectus. In in eros felis. Suspendisse euismod neque non bibendum fringilla. Cras aliquam, arcu ut ultrices venenatis, ipsum sem suscipit odio, iaculis viverra dolor ante eu nunc. Pellentesque tempor ipsum ut tempor cursus. Integer in ligula varius dolor pellentesque luctus et vitae est. Sed imperdiet massa metus. Mauris malesuada tristique purus non fermentum. Praesent mattis eros et ornare congue. 
+        </p>
+    </section>
+    
+    <footer>
+    	<p>George Trammell Media&trade; &copy;2014</p>
+    </footer>
+</body>
+</html>
