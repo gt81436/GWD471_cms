9f2d2a84e3bb4fba8250901e30154960782f6cfe
gt81436 <george.trammell@yahoo.com>
2014-04-09 20:39:41 -0400

    Added Kobe

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <section>
     	<h2>Intro</h2>
         <p>
-        <img src="" alt="my pic" />
+        <img src="images/kobe.jpg" alt="my pic" />
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis purus, pharetra eu ante nec, sollicitudin interdum tortor. Suspendisse in dui lectus. In in eros felis. Suspendisse euismod neque non bibendum fringilla. Cras aliquam, arcu ut ultrices venenatis, ipsum sem suscipit odio, iaculis viverra dolor ante eu nunc. Pellentesque tempor ipsum ut tempor cursus. Integer in ligula varius dolor pellentesque luctus et vitae est. Sed imperdiet massa metus. Mauris malesuada tristique purus non fermentum. Praesent mattis eros et ornare congue. 
         </p>
     </section>
