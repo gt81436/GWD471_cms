203da8a337d9b1cc9bc52bcebf9a7064a1c080f8
thb <george.trammell@yahoo.com>
2014-04-17 01:22:30 +0000

    added status indication

--- about.php
+++ about.php
@@ -1,10 +1,10 @@
-  <?php $page_id = 'home'; ?>
+ <?php $page_id = 'about'; ?>
   <?php require_once('includes/top.php');?>
   <?php require_once('includes/header.php');?>
     <section>
-    	<h2>Intro</h2>
+    	<h2>About</h2>
         <p>
-        <img src="images/kobe.jpg" alt="my pic" />
+        
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis purus, pharetra eu ante nec, sollicitudin interdum tortor. Suspendisse in dui lectus. In in eros felis. Suspendisse euismod neque non bibendum fringilla. Cras aliquam, arcu ut ultrices venenatis, ipsum sem suscipit odio, iaculis viverra dolor ante eu nunc. Pellentesque tempor ipsum ut tempor cursus. Integer in ligula varius dolor pellentesque luctus et vitae est. Sed imperdiet massa metus. Mauris malesuada tristique purus non fermentum. Praesent mattis eros et ornare congue. 
         </p>
     </section>
