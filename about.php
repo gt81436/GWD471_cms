3fe84b342295fcc66a2420dc8a9d88d2adc5437e
thb <george.trammell@yahoo.com>
2014-05-08 01:08:48 +0000

    fixed our product display and created admin section

--- about.php
+++ about.php
@@ -1,10 +1,36 @@
- <?php $page_id = 'about'; ?>
+  <?php $page_id = 'about'; ?>
+  <?php require_once('../../config.php');?>
+  <?php
+  //interact with DB
+  $sql = 'SELECT * FROM media';
+
+  //send coomand to MySQL
+  $myData = $db->query($sql)
+  OR exit('unable to select data from table');
+
+  //close DB connection
+  $db->close();
+  ?>
+
   <?php require_once('includes/top.php');?>
   <?php require_once('includes/header.php');?>
     <section>
-    	<h2>About</h2>
+    	<h2>About Us</h2>
+  <?php
+    echo '<div id="products">';
+    while($row = $myData->fetch_assoc())
+    {
+      echo '<div class="product">';
+      echo '<img src="" alt="" />';
+      echo '<h3>' . $row['name'] . '</h3>';
+      echo '<p>' . $row['description'] . '</p>';
+      echo '<p>$' . $row['price'] . '</p>';
+      echo '</div>';
+    }
+    echo '<br class="clear" />';
+    echo '</div>'
+  ?>
         <p>
-        
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis purus, pharetra eu ante nec, sollicitudin interdum tortor. Suspendisse in dui lectus. In in eros felis. Suspendisse euismod neque non bibendum fringilla. Cras aliquam, arcu ut ultrices venenatis, ipsum sem suscipit odio, iaculis viverra dolor ante eu nunc. Pellentesque tempor ipsum ut tempor cursus. Integer in ligula varius dolor pellentesque luctus et vitae est. Sed imperdiet massa metus. Mauris malesuada tristique purus non fermentum. Praesent mattis eros et ornare congue. 
         </p>
     </section>
--- contact.php
+++ contact.php
@@ -1,14 +1,104 @@
- <?php $page_id = 'contact'; ?>
-  <?php require_once('includes/top.php');?>
-  <?php require_once('includes/header.php');?>
+ <?php
+
+    if(@$_POST['submit'])
+    {
+      require_once('includes/validation.php');
+      
+      $name = trim($_POST['name']);
+      $email = trim($_POST['email']);
+      $comments = trim($_POST['comments']);
+      $errors = array();
+      
+      // test user info for non-valid data
+      $valid = verifyAlphaNum($name);
+      if(!$valid)
+      {
+         $errors['name'] = "Name must be letter, numbers, or spaces"; 
+      }
+      
+      $valid = verifyEmail($email);
+      if(!$valid)
+      {
+        $errors['email'] = "Please provide a vaild email address - test@test.com";
+      }
+      
+      $comments = verifyText($comments);
+	
+	    //if any errors, don't send email - else send email
+	    if(count($errors) === 0)
+	    {
+          //prepare for sending email
+          $destination = 'george.trammell@yahoo.com';
+          $subject = 'Comments from my website';
+          $body = "$name \n $email \n $comments";
+      
+        if(mail($destination, $subject, $body))
+        {
+          echo 'Your message has been sent. Below is the info you provided:';
+          
+          //test to see if form data is recieved.
+          echo '<h1>Thanks for providing feedback</h1>';
+          echo '<ul>';
+          echo '<li>' . $_POST['name'] . '</li>';
+          echo '<li>' . $_POST['email'] . '</li>';
+          echo '<li>' . $_POST['comments'] . '</li>';
+          echo '</ul>';
+        }
+        else
+        {
+          echo 'There was an error. Your message has NOT been sent.';
+        }
+	    }//if count end
+
+    }
+    else // form not submitted
+    {  
+      //echo "just view form";
+    }
+
+  ?>
+
+
+  <?php $page_id = 'contact'; ?>
+  <?php require_once('includes/top.php'); ?>
+  <?php require_once('includes/header.php'); ?>
+    
     <section>
     	<h2>Contact</h2>
-        <p>
-        
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis purus, pharetra eu ante nec, sollicitudin interdum tortor. Suspendisse in dui lectus. In in eros felis. Suspendisse euismod neque non bibendum fringilla. Cras aliquam, arcu ut ultrices venenatis, ipsum sem suscipit odio, iaculis viverra dolor ante eu nunc. Pellentesque tempor ipsum ut tempor cursus. Integer in ligula varius dolor pellentesque luctus et vitae est. Sed imperdiet massa metus. Mauris malesuada tristique purus non fermentum. Praesent mattis eros et ornare congue. 
-        </p>
+      
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <fieldset>
+          <legend>Contact Us</legend>
+          <p>
+            <label for="name">Name: </label>
+            <span class="errors"><?php echo @$errors['name']; ?></span>
+            <input type="text" name="name" placeholder="Type your name here"
+             value="<?php echo @$_POST['name']; ?>"/>
+          </p>
+          
+          <p>
+            <label for="email">Email: </label>
+             <span class="errors"><?php echo @$errors['email']; ?></span>
+            <input type="text" name="email" placeholder="Type your email here"
+             value="<?php echo @$_POST['email']; ?>"/>
+          </p>
+          
+          <p>
+            <label for="email">Comments: </label>
+            <textarea name="comments" placeholder="type your message here">
+            <?php echo @$_POST['comments']; ?></textarea>
+          </p>
+          
+          <p>
+            <input type="submit" value="send feedback now" name="submit" />
+          </p>
+        </fieldset>
+      </form>
+       
     </section>
     
-  <?php require_once('includes/footer.php');?>
+  <?php require_once('includes/footer.php'); ?>
+    
 </body>
 </html>
+
